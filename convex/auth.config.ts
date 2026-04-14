81030701beafffdf2bd878dc11157a71233afff5
Rajat Roy <rajatroy977@gmail.com>
2026-04-15 01:56:37 +0530

    Initial commit

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,7 +1,8 @@
 const config = {
     providers: [
         {
-            domain: "https://emerging-camel-41.clerk.accounts.dev/",
+            // domain: "https://nice-beagle-13.clerk.accounts.dev",
+            domain:"https://emerging-camel-41.clerk.accounts.dev",
             applicationID: "convex",
         },
     ]
